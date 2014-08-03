3cf55180aec665757a0f13199fb12fd4a15cf2b1
slhowe <sarahlhowe@gmail.com>
2014-08-03 23:21:35 +1200

    lastTime update after calculations

--- whisker/whisker.ino
+++ whisker/whisker.ino
@@ -1,7 +1,6 @@
 /* 
-** interrupt on falling edge -> count++
-** polling notes current time, count, prevtime
-** calc dtime, divide to get freq
+** Frequency calculator for whisker board
+** written by Sarah Howe
 */ 
 
 #define CONV 250000 // for 1 clock tick every 4us (250kHz clock)
@@ -16,13 +15,11 @@ void setup()
 {
   Serial.begin(9600);
   attachInterrupt(0, WISR, FALLING); //enable interrupt0 (pin2)
-  //TIMSK1=0x01; // enabled global and timer overflow interrupt;
   TCCR1A = 0x06; // 64x prescale for 250kHz clock
   TCNT1=0x0000; // 16bit counter register initialised to 0
-  //TCCR1B = 0x06; // start timer/ set clock
 }
 
-void WISR()
+void WISR() //WhiskerInterruptServiceRoutine
 {
   count++; 
 }
@@ -32,11 +29,12 @@ void loop()
   noInterrupts(); //critical section
   time = TCNT1;
   interrupts();
-  deltaTime = time - lastTime; //time ifference between last poll
+  deltaTime = time - lastTime; //time difference between last poll
   if(deltaTime > 0) //-ve if overflow and no NaN
   {
     freq = (count*CONV)/deltaTime; //calc frequency
   }
+  lastTime = time; //<-- fuck up if dt = 0;
   //Serial.print(count-lastCount);
   Serial.println(freq);
   //lastCount = count;
