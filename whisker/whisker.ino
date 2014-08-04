0935586300e472a978f110f461142dd1dcfe6d8f
slhowe <sarahlhowe@gmail.com>
2014-08-04 12:20:51 +1200

    deltaTime now constant for 16MHz clock div 64x

--- whisker/whisker.ino
+++ whisker/whisker.ino
@@ -5,18 +5,19 @@
 
 #define CONV 250000 // for 1 clock tick every 4us (250kHz clock)
 
-volatile int count = 0;
-int pulses = 0;
-int lastTime = 0;
-int time = 0;
-int deltaTime = 0;
-long freq = 0;
+volatile unsigned int count = 0;
+unsigned int pulses = 0;
+unsigned int lastTime = 0;
+unsigned int time = 0;
+unsigned int deltaTime = 0;
+unsigned long freq = 0;
 
 void setup()
 {
   Serial.begin(9600);
   attachInterrupt(0, WISR, FALLING); //enable interrupt0 (pin2)
-  TCCR1A = 0x06; // 64x prescale for 250kHz clock
+  TCCR1A = 0x00; // normal operation mode
+  TCCR1B = 0x03; // 64x prescale for 250kHz clock
   TCNT1=0x0000; // 16bit counter register initialised to 0
 }
 
@@ -32,12 +33,13 @@ void loop()
   pulses = count; //???
   interrupts();
   deltaTime = time - lastTime; //time difference between last poll
-  if(deltaTime > 0) //-ve if overflow and no NaN
+  if(deltaTime < 60000) //-ve if overflow and no NaN
   {
     freq = (pulses*CONV)/deltaTime; //calc frequency
   }
+  count = 0; //reset count
   lastTime = time; //<-- fuck up if dt = 0;
   //Serial.print(count-lastCount);
-  Serial.println(freq);
+  Serial.println(pulses);
   //lastCount = count;
 }
