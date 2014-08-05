d464e7e34e406c7e11145f90eeba36827994244f
slhowe <sarahlhowe@gmail.com>
2014-08-05 12:25:56 +1200

    new and improved interrupts

--- whisker/whisker.ino
+++ whisker/whisker.ino
@@ -2,11 +2,13 @@
 ** Frequency calculator for whisker board
 ** written by Sarah Howe
 */ 
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-#define CONV 250000 // for 1 clock tick every 4us (250kHz clock)
+#define CONV 250000 //for 1 clock tick every 4us (250kHz clock)
 
-volatile unsigned int count = 0;
-unsigned int pulses = 0;
+volatile unsigned int count = 27;
+unsigned int pulses = 27;
 unsigned int lastTime = 0;
 unsigned int time = 0;
 unsigned int deltaTime = 0;
@@ -15,15 +17,20 @@ unsigned long freq = 0;
 void setup()
 {
   Serial.begin(9600);
-  attachInterrupt(0, WISR, FALLING); //enable interrupt0 (pin2)
-  TCCR1A = 0x00; // normal operation mode
-  TCCR1B = 0x03; // 64x prescale for 250kHz clock
-  TCNT1=0x0000; // 16bit counter register initialised to 0
+  //attachInterrupt(0, WISR, FALLING); //enable interrupt0 (pin2)
+  //pinMode(0, INPUT);
+  cli(); //disable glob interrupts
+  TCCR1A = 0x00; //normal operation mode
+  TCCR1B = 0x03; //64x prescale for 250kHz clock
+  TCNT1=0x0000; //16bit counter register initialised to 0
+  EIMSK |=(1<<INT0); //interrupt 0 (pin 2)
+  EICRA |=(1<<ISC01); //falling edge
+  sei(); //enable glob interrupts
 }
 
-void WISR() //WhiskerInterruptServiceRoutine
+ISR(EXT_INT0_vect)
 {
-  count++; 
+    count = 0;
 }
 
 void loop()
@@ -37,9 +44,10 @@ void loop()
   {
     freq = (pulses*CONV)/deltaTime; //calc frequency
   }
-  count = 0; //reset count
+  //count = 0; //reset count
   lastTime = time; //<-- fuck up if dt = 0;
   //Serial.print(count-lastCount);
-  Serial.println(pulses);
+  Serial.println(count);
+  //Serial.println("hello");
   //lastCount = count;
 }
