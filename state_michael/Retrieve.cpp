5a7a77cecbc72f6d69ef04147bee64920491add1
slhowe <sarahlhowe@gmail.com>
2014-08-07 13:35:35 +1200

    full implementable (unrun)

--- state_michael/Retrieve.cpp
+++ state_michael/Retrieve.cpp
@@ -1,23 +1,10 @@
-#define CW 1
-#define CCW -1
-
-unsigned int target = Wavearm.currentAngle;
-unsigned int rotation = 0;
-unsigned int current = Magno.currentAngle;
-unsigned int p_rotation = 0;
-unsigned int motor_speed = 0;
-
-int P = 0;
-int D = 0;
-int KP = 1;
-int KD = 1;
-
+#include "Retrieve.h"
 
 //Aligns the robot to a detected weight
 
 bool align(void){
-	//since arm on the right, assume target always clockwise of current
-	//input detector arm if 2 are used to determine dir to turn
+	//since arm on the right, assume target always clockwise of current location
+	//input detector arm triggered if 2 are used to determine dir to turn
 	aligned = false;
 	//check arm pos, note mag pos
 	//change pos, check mag, change by D_armpos
@@ -36,7 +23,7 @@ bool align(void){
 	D = (rotation - p_rotation); //-->0
 	
 	//as a percentage
-	motor_speed = 90 + (KP*P + KD*D);
+	motor_speed = (KP*P + KD*D)*100/FULLSPEED;
 	Motors.turn(motor_speed, CW)	
 			
 	if(rotation == target){
