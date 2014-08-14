304555f4ab4c296216b2b2efaef1c799a626d458
slhowe <sarahlhowe@gmail.com>
2014-08-15 09:34:17 +1200

    new filtering on whisker

--- state_michael/Whisker.cpp
+++ state_michael/Whisker.cpp
@@ -12,52 +12,59 @@ Whisker::Whisker(void){
 
 
 bool Whisker::detect (bool quick){
-  _prevObject = object;
-  cli(); //critical section
-  time = TCNT1;
-  pulses = count; 
-  sei();
-  deltaTime = time - lastTime; //time difference between last poll
-  if(deltaTime < MAX_TIME){ //-ve if overflow and no NaN
-    freq = (pulses*CONV)/deltaTime; 
-  }
-  count = 0; //reset count
-  lastTime = time;
-//  Serial.print("freq: ");
-  Serial.println(freq);
-  if (freq < DETECT_THRESHOLD){
-    object = true;    
-  }
-  else{
-    object = false;
-  }  
-  if(quick){
-   return object; 
-  }
-  if(doubleCheck(object)){
-    _noChange = object;
-    return object; 
-  }
-  else{
-    return _noChange;
-  }
+	//_prevObject = object;
+	cli(); //critical section
+	time = TCNT1;
+	pulses = count; 
+	sei();
+	deltaTime = time - lastTime; //time difference between last poll
+	if(deltaTime < MAX_TIME){ //-ve if overflow and no NaN
+		if (deltaTime == 0) {
+			return -1;
+		}else{
+			freqRead = (pulses*CONV)/deltaTime; 
+		}
+	}
+	//count = 0; //reset count
+	//lastTime = time;
+	//Serial.print("freq: ");
+	//Serial.println(freqRead);
+	
+	filter_reg = filter_reg - (filter_reg >> FILTER_SHIFT) + freqRead; 
+	freq = filter_reg >> FILTER_SHIFT;
+	
+	if (freq < DETECT_THRESHOLD){
+		object = true;    
+	}else{
+		object = false;
+	}  
+	return object;
+	//if(quick){
+	//	return object; 
+	//}
+	//if(doubleCheck(object)){
+	//	_noChange = object;
+	//	return object; 
+	//}else{
+	//	return _noChange;
+	//}
 }
 
 bool Whisker::doubleCheck(bool object) {
-  //Serial.print("sc: ");
-  //Serial.println(_sureCount);
-  if(_sureCount >= TOLERANCE){
-     _sureCount = 0;
-    return true; 
-  }
-  else if(object == _prevObject){
-     _sureCount++; 
-     return false;
-  }
-  else{
-    _sureCount = 0;
-    return false;
-  }
+	//Serial.print("sc: ");
+	//Serial.println(_sureCount);
+	if(_sureCount >= TOLERANCE){
+		_sureCount = 0;
+		return true; 
+	}
+	else if(object == _prevObject){
+		_sureCount++; 
+		return false;
+	}
+	else{
+		_sureCount = 0;
+		return false;
+	}
 }
 
-  
+
--- state_michael/Whisker.h
+++ state_michael/Whisker.h
@@ -10,7 +10,7 @@
 #define DETECT_THRESHOLD 53000
 #define QUICK true
 #define SLOW false
-
+#define FILTER_SHIFT 2
 #define TOLERANCE 8
 
 class Whisker
@@ -18,19 +18,21 @@ class Whisker
 	public:
 		Whisker(void);
 		//void WISR(void);		
-                bool detect(bool quick);
+		bool detect(bool quick);
 		bool object;
 		volatile unsigned int count;
 	private:
-                bool doubleCheck(bool object);
+		bool doubleCheck(bool object);
 		unsigned int pulses;
 		unsigned int lastTime;
 		unsigned int time;
 		unsigned int deltaTime;
 		unsigned long freq;
-                unsigned long _sureCount;
-                bool _prevObject;
-                bool _noChange;
+		//unsigned long _sureCount;
+		//bool _prevObject;
+		//bool _noChange;i
+		long long filter_reg;
+		unsigned long freqRead;
 };
 
 #endif
