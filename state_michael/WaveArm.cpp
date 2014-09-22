17fa8a292e53aeb63e7c9afc31ac34c08cbc7843
slhowe <sarahlhowe@gmail.com>
2014-09-23 10:43:44 +1200

    not really sure what I changed, it was yesterday

--- state_michael/WaveArm.cpp
+++ state_michael/WaveArm.cpp
@@ -2,8 +2,11 @@
 #include "WaveArm.h"
 /* Functions to move the sweeper arms */
 
+#define CONV 250000
 #define ARMS_IN 1
 #define ARMS_OUT 0
+#define ANGLE_MAX 180
+#define ANGLE_MIN 0
 
 WaveArm::WaveArm(void)
 {
@@ -12,38 +15,42 @@ WaveArm::WaveArm(void)
 }
 
 bool WaveArm::sweepIn(Servo sweepArmLeft, Servo sweepArmRight){
-	if(angle<180){
+	if(angle<ANGLE_MAX){
 		sweepArmLeft.write(angle);
-		sweepArmRight.write(angle);
+		sweepArmRight.write(ANGLE_MAX - angle);
 		angle += 1;
 	}else{
-		return 1;
+		return ARMS_IN;
 	}
 }
 
 bool WaveArm::sweepOut(Servo sweepArmLeft, Servo sweepArmRight){
-	if(angle>0){
+	if(angle>ANGLE_MIN){
 		sweepArmLeft.write(angle);
-		sweepArmRight.write(angle);
+		sweepArmRight.write(ANGLE_MAX - angle);
 		angle -= 1;
 	}else{
-		return 0;
+		return ARMS_OUT;
 	}
 }
 
 //250kHz clock input is in ms delay between movement 6 optimal, 3 max
-void WaveARm::collect(int speed_ms){
+void WaveArm::collect(int speed_ms){
 	curr_time = ticks;
 	delta_ms = (curr_time-prev_time)/CONV;
-	if(delta_ms>speed_ms && ARMS_OUT){
-		sweepIn(sweepArmLeft, sweepArmRight);
+	if(delta_ms>speed_ms && armLocation==ARMS_OUT){
+		armLocation = sweepIn(sweepArmLeft, sweepArmRight);
 		prev_time = curr_time;
 	}
-	if(not_collected()){
-		wait;
-	}
-	if(delta_ms>speed_ms && ARMS_IN){
-		sweepOut(sweepArmLeft, SweepArmRight);
+	//if(not_collected()){
+	//	wait;
+	//}
+	if(delta_ms>speed_ms && armLocation==ARMS_IN){
+		armLocation = sweepOut(sweepArmLeft, SweepArmRight);
 		prev_time = curr_time;
 	}
+}
+
+void WaveArm::knockOver(){
+	
 }
