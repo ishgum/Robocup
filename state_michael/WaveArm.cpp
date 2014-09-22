b9784ceee938a50c91b6e4ef60a9d9c15800c173
slhowe <sarahlhowe@gmail.com>
2014-09-23 11:13:35 +1200

    arms cleaned up to a possibly runnable state

--- state_michael/WaveArm.cpp
+++ state_michael/WaveArm.cpp
@@ -7,11 +7,11 @@
 #define ARMS_OUT 0
 #define ANGLE_MAX 180
 #define ANGLE_MIN 0
+#define SPEED_MS 6
 
 WaveArm::WaveArm(void)
 {
-  currentAngle = 0;
-  currentDirection = ARM_CLOCKWISE;
+  angle = 0;
 }
 
 bool WaveArm::sweepIn(Servo sweepArmLeft, Servo sweepArmRight){
@@ -35,17 +35,17 @@ bool WaveArm::sweepOut(Servo sweepArmLeft, Servo sweepArmRight){
 }
 
 //250kHz clock input is in ms delay between movement 6 optimal, 3 max
-void WaveArm::collect(int speed_ms){
-	curr_time = ticks;
+void WaveArm::collect(void){
+	curr_time = TCNT1;
 	delta_ms = (curr_time-prev_time)/CONV;
-	if(delta_ms>speed_ms && armLocation==ARMS_OUT){
+	if(delta_ms>SPEED_MS && armLocation==ARMS_OUT){
 		armLocation = sweepIn(sweepArmLeft, sweepArmRight);
 		prev_time = curr_time;
 	}
 	//if(not_collected()){
 	//	wait;
 	//}
-	if(delta_ms>speed_ms && armLocation==ARMS_IN){
+	if(delta_ms>SPEED_MS && armLocation==ARMS_IN){
 		armLocation = sweepOut(sweepArmLeft, SweepArmRight);
 		prev_time = curr_time;
 	}
@@ -53,4 +53,8 @@ void WaveArm::collect(int speed_ms){
 
 void WaveArm::knockOver(){
 	
+}
+
+bool WaveArm::collected(void){
+
 }
--- state_michael/WaveArm.h
+++ state_michael/WaveArm.h
@@ -4,24 +4,31 @@
 
 #include "Arduino.h"
 
-#define ARM_CLOCKWISE 1
-#define ARM_ANTI -1
+#define CONV 250000
+#define ARMS_IN 1
+#define ARMS_OUT 0
+#define ANGLE_MAX 180
+#define ANGLE_MIN 0
+#define SPEED_MS 6
 
-#define MAX_ANGLE 130
-#define MIN_ANGLE 50
+/*Control functions for both sweeper arms together
+  Control for knockover arms (?)
+  Detection of weights within robot */
 
-
-class WaveArm
-{
 	public:
 		WaveArm (void);
-                void armPos(void);
-                
-                
-                unsigned int currentAngle;
-                unsigned int currentDirection;
+        void collect(void);        
+		void knockover(void);
+        unsigned int angle;
+
 	private:
-               
+		void sweepIn(Servo sweepArmLeft, Servo sweepArmRight);
+		void sweepOut(Servo sweepArmLeft, Servo sweepArmRight);
+		bool collected(void);
+		unsigned long curr_time;
+		unsigned long prev_time;
+		unsigned long delta_ms;
+		int armLocation;		
 };
 
 #endif
