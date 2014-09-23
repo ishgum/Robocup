817b6fe2dd9c6f63d9ff6a8f222a134b770743d1
slhowe <sarahlhowe@gmail.com>
2014-09-24 11:42:22 +1200

    whisker is doing its thing

--- state_michael/state_michael.ino
+++ state_michael/state_michael.ino
@@ -7,35 +7,47 @@
  http://arduino.cc/en/Tutorial/Sweep
 */ 
 
+#include "Arduino.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
 #include <Servo.h> 
 #include "WaveArm.h"
 #include "Servo.h"
+#include "Whisker.h"
 
 volatile unsigned long tick = 0;
 Servo sweepArmLeft;   
 Servo sweepArmRight;    
 WaveArm sweeperArms; 
 
+Whisker whisker;
 
 void setup() 
 { 
   Serial.begin(9600);
-//  cli();
-    
-//	TCCR1A = 0x00; //normal operation mode
-//	TCCR1B = 0x03; //64x prescale for 250kHz clock
-//	TCNT1=0x0000; //16bit counter register initialised to 0
+  cli();
+        attachInterrupt(5, WISR, FALLING);
+        pinMode(18, INPUT); //green 19 is yellow
+	TCCR1A = 0x00; //normal operation mode
+	TCCR1B = 0x03; //64x prescale for 250kHz clock
+	TCNT1=0x0000; //16bit counter register initialised to 0
 
 //        TCCR2A = 0x00; //normal mode
 //        TCCR2B = 0x04; //64 prescale for 1ms overflow rate
 //        TCNT2 = 0x0000; //Zero it
 //        TIMSK2 = (1<<TOIE2); //interrupt on TCNT2 overflow
-//  sei(); 
+  sei(); 
   
 	sweepArmLeft.attach(11); 
 	sweepArmRight.attach(10); 
 
 } 
+
+void WISR(void)
+{
+    whisker.count++;
+}
 
 //ISR(TIMER2_OVF_VECT){
 //   tick++; 
@@ -45,7 +57,7 @@ void loop()
 {
 
 sweeperArms.collect(sweepArmLeft, sweepArmRight);
-Serial.println(millis());
+whisker.detect();
 
 } 
 
